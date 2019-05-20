3646c925dc04919d36b6c67e40272138d28eeb93
Filip Dreger <filip.x.dreger@ericsson.com>
2019-05-20 15:39:41 +0200

    Begin implementing reset

---
+++ src/main/java/net/snowyhollows/bento2/BentoResettable.java
@@ -0,0 +1,5 @@
+package net.snowyhollows.bento2;
+
+public interface BentoResettable {
+	void reset(Bento bento);
+}
---
+++ src/main/java/net/snowyhollows/bento2/annotation/Reset.java
@@ -0,0 +1,11 @@
+package net.snowyhollows.bento2.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target(ElementType.PARAMETER)
+public @interface Reset {
+}
