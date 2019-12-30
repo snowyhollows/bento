a35a2ddece6bfe53b2afbbf83b32b51a08541079
Filip Dreger <filip.x.dreger@ericsson.com>
2019-12-30 15:40:37 +0100

    Align with bento-gerenerator 1.2

--- src/main/java/net/snowyhollows/bento2/annotation/ByFactory.java
+++ src/main/java/net/snowyhollows/bento2/annotation/ByFactory.java
@@ -8,7 +8,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.PARAMETER)
+@Target({ElementType.PARAMETER, ElementType.METHOD})
 public @interface ByFactory {
     Class<? extends BentoFactory> value() default DefaultFactory.class;
 }
--- src/main/java/net/snowyhollows/bento2/annotation/ByName.java
+++ src/main/java/net/snowyhollows/bento2/annotation/ByName.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
-@Target(ElementType.PARAMETER)
+@Target({ElementType.PARAMETER, ElementType.METHOD})
 public @interface ByName {
     String value() default "##";
 }
